0632b7d3264a7574e7b1f1ce6fbdd45a106aa8f8
weibinw219 <weibinw0219@gmail.com>
2021-01-07 01:53:27 -0500

    Add sticky navigation bar

--- js/app.js
+++ js/app.js
@@ -5,14 +5,26 @@ const projectsSection = document.querySelector('#projects-section')
 const contactSection = document.querySelector('#contact-section')
 const aboutSection = document.querySelector('#header-section')
 
+const navbar = document.querySelector('.nav-wrapper')
+const sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+window.onscroll = function () { myFunction() };
 
 for (let i = 0; i < navItems.length; i++) {
     navItems[i].addEventListener('click', function (event) {
         event.preventDefault();
         const sectionName = navItems[i].getAttribute('href')
-        console.log(sectionName)
+        console.log(navItems[i].scrollHeight)
         window.scroll({
-            top: getOffset(document.querySelector(`${sectionName}-section`)).top,
+            top: (getOffset(document.querySelector(`${sectionName}-section`)).top - navItems[i].scrollHeight),
             behavior: 'smooth'
         });
     })
